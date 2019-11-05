16411ac04b173b77c39820118550569098565733
Chu <mouse170@gmail.com>
2019-11-05 13:37:58 +0800

    實作reuse函式

---
+++ ch04/scripts/utils.js
@@ -0,0 +1,12 @@
+//add
+function add(x, y) {
+  return x + y;
+}
+//gettatal
+function getTotal(min, max) {
+  var total = 0;
+  for (var i = min; i <= max; i++) {
+    total += i;
+  }
+  return total;
+}

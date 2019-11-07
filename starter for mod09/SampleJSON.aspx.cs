9905c058d17bf92da80f662ea89285b693f7cdb1
Chu <mouse170@gmail.com>
2019-11-07 15:39:41 +0800

    move aspxfiles 實作CH09

---
+++ starter for mod09/SampleJSON.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class SampleJSON : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Response.Clear();
+        Response.ContentType = @"application/json";
+        string s = Request.Params["filter"] != null ? Request.Params["filter"].ToLower() : "all";
+        if (s == "all")
+        {
+            Response.WriteFile(Server.MapPath("dataAll.json"));
+        }
+        else
+        {
+            Response.WriteFile(Server.MapPath("data.json"));
+        }
+        Response.End();
+    }
+}
---
+++ starter for mod09/SampleXML.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class SampleXML : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Response.Clear();
+        Response.ContentType = "text/xml";
+        Response.Write("<?xml version='1.0'?><employees><employee><id>1</id><name>mary</name></employee ></employees>");
+        Response.End();
+    }
+}

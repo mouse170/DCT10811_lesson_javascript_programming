539ab62c7e2213dd75355c85ff7f7420dad4f7e2
Chu <mouse170@gmail.com>
2019-11-07 15:02:21 +0800

    COPY Data files

---
+++ ch09/SampleXML.aspx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class SampleXML : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Response.Clear();
+        Response.Write("<h1>I am Sample.aspx -- " + DateTime.Now.Millisecond + "</h1>");
+        Response.End();
+    }
+}
